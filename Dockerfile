3f09fdc74ea416694f272dcd76a70e8bc64585c6
Sai Kaushik Mudiki <mudiki08@gmail.com>
2021-01-26 02:34:55 -0500

    updated the service

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM python:3
 ADD python-sample.py /
-RUN pip install flask
-RUN pip install flask_restful
+RUN pip install -r requirements.txt --user
 EXPOSE 3333
 CMD [ "python", "./python-sample.py"]
--- python-sample.py
+++ python-sample.py
@@ -8,16 +8,53 @@
 #str = 'python program'
 #print('This is Sample, ' + str + '!')
 from flask import Flask, request
-from flask_restful import Resource, Api
+#from flask_restful import Resource, Api
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
 
-class Greeting (Resource):
-    def get(self):
-        return 'This is a sample python service'
+app.config['MONGO_DBNAME'] = 'taskdb'
+app.config['MONGO_URI'] = 'mongodb+srv://administrator:wvbDjPOAXRlUEqRm@taskcluster.rf38o.mongodb.net/taskdb?retryWrites=true&w=majority'
 
-api.add_resource(Greeting, '/') # Route_1
+mongo = PyMongo(app)
+
+@app.route('/task', methods=['GET'])
+def get_all_stars():
+  task = mongo.db.tasks
+  output = []
+  for t in task.find():
+    output.append({'name' : t['name'], 'status' : t['status']})
+  return jsonify({'result' : output})
+
+@app.route('/singletask/', methods=['GET'])
+def get_one_task(name):
+  task = mongo.db.tasks
+  t = task.find_one({'name' : name})
+  if t:
+    output = {'name' : t['name'], 'status' : t['status']}
+  else:
+    output = "No such task"
+  return jsonify({'result' : output})
+
+@app.route('/addtask', methods=['POST'])
+def add_task():
+  task = mongo.db.tasks
+  name = request.json['name']
+  status = request.json['status']
+  task_id = task.insert({'name': name, 'status': status})
+  new_task = task.find_one({'_id': task_id })
+  output = {'name' : new_task['name'], 'status' : new_task['status']}
+  return jsonify({'result' : output})
+
+# class Greeting (Resource):
+#     def get(self):
+#         return 'This is a sample task service'
+
+# api.add_resource(Greeting, '/') # Route_1
 
 if __name__ == '__main__':
     app.run('0.0.0.0','3333')
